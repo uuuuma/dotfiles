e158e97cb2fb6e3bb308d8c53e9850237cc7b22e
uuuuma <94543703+uuuuma@users.noreply.github.com>
2023-04-01 17:34:22 +0900

    feat: add macos settings

--- macos.zsh
+++ macos.zsh
@@ -23,6 +23,12 @@ defaults write "Apple Global Domain" "InitialKeyRepeat" -int 15
 # shorten "key repeat rate" to maximum
 defaults write "Apple Global Domain" "KeyRepeat" -int 2
 
+# disable auto-period when input double-space
+defaults write "Apple Global Domain" "NSAutomaticPeriodSubstitutionEnabled" -int 0
+
+# disable auto-capitalization
+defaults write "Apple Global Domain" "NSAutomaticCapitalizationEnabled" -int 0
+
 # disable live conversion in Japanese input
 defaults write "com.apple.inputmethod.Kotoeri" "JIMPrefLiveConversionKey" -int 0
 
@@ -31,6 +37,17 @@ defaults write "com.apple.inputmethod.Kotoeri" "JIMPrefCharacterForYenKey" -int 
 
 # show Emoji & Symbols when enter the fn key
 defaults write "com.apple.HIToolbox" "AppleFnUsageType" -int 2
+
+# disable input-sources shortcuts (ctrl+space & ctrl+shift+space)
+defaults write "com.apple.symbolichotkeys" "AppleSymbolicHotKeys" -dict-add 60 "<dict><key>enabled</key><false/></dict>"
+defaults write "com.apple.symbolichotkeys" "AppleSymbolicHotKeys" -dict-add 61 "<dict><key>enabled</key><false/></dict>"
+
+# disable spotlight keyboard shortcuts (command+space & command+shift+space)
+defaults write "com.apple.symbolichotkeys" "AppleSymbolicHotKeys" -dict-add 64 "<dict><key>enabled</key><false/></dict>"
+defaults write "com.apple.symbolichotkeys" "AppleSymbolicHotKeys" -dict-add 65 "<dict><key>enabled</key><false/></dict>"
+
+# hide the spotlight icon from the menu bar
+defaults delete "com.apple.Spotlight" "NSStatusItem Visible Item-0"
 
 # mute startup sound
 sudo nvram StartupMute="%01"
